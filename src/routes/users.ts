374e04912914a77b2dfad7cc0a280287acf1ad0b
Arimario Jesus <arimario.jesus@hotmail.com>
2022-01-26 23:35:06 -0300

    feat: add users put and delete routes

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -21,4 +21,17 @@ usersRoute.post('/:uuid', (req: Request, res: Response, next: NextFunction) => {
   res.status(201).json(newUser);
 });
 
+usersRoute.put('/:uuid', (req: Request, res: Response, next: NextFunction) => {
+  const uuid = parseInt(req.params.uuid);
+  const modifiedUser = req.body;
+
+  modifiedUser.uuid = uuid;
+
+  res.status(200).json(modifiedUser);
+});
+
+usersRoute.delete('/:uuid', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200);
+});
+
 export default usersRoute;
