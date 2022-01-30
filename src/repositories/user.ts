192dfafa0a13eb0a6f8a9748b7c2db1aaa713686
Arimario Jesus <arimario.jesus@hotmail.com>
2022-01-29 21:40:09 -0300

    feat: add update method to user entity

--- src/repositories/user.ts
+++ src/repositories/user.ts
@@ -46,6 +46,19 @@ class UserRepository {
     
     return newUser.uuid;
   }
+
+  async update(user: User): Promise<void> {
+    const script = `
+      UPDATE INTO application_user 
+      SET 
+        username = $1,
+        password = crypt($2, 'my_salt')
+      WHERE uuid = $3
+    `;
+
+    const values = [user.username, user.password];
+    await db.query(script, values);
+  }
 }
 
 export default new UserRepository();
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -23,13 +23,20 @@ usersRoute.post('/:uuid', async (req: Request, res: Response, next: NextFunction
   res.status(201).send(uuid);
 });
 
-usersRoute.put('/:uuid', (req: Request, res: Response, next: NextFunction) => {
-  const uuid = parseInt(req.params.uuid);
-  const modifiedUser = req.body;
+usersRoute.put('/:uuid', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const uuid = parseInt(req.params.uuid);
+    const modifiedUser = {
+      ...req.body,
+      uuid,
+    };
 
-  modifiedUser.uuid = uuid;
+    await userRepository.update(modifiedUser);
 
-  res.status(200).json(modifiedUser);
+    res.status(200).send();
+  } catch {
+    res.status(400).send('Error updating user');
+  }
 });
 
 usersRoute.delete('/:uuid', (req: Request, res: Response, next: NextFunction) => {
